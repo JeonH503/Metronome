9444cf49a4066a5b5ea9aaab913644c7ed910a61
JunHui Jeon <52855980+JeonH503@users.noreply.github.com>
2023-03-08 17:16:39 +0900

    buffer node로 변경

--- src/Metronome/Component/BeatCell.js
+++ src/Metronome/Component/BeatCell.js
@@ -1,27 +1,16 @@
 class BeatCell {
-    constructor(outputNode, audioElement) {
+    constructor(outputNode, audioBuffer) {
         this._context = outputNode.context;
-        // this._buffer = audioBuffer;
-        this._element = audioElement
+        this._buffer = audioBuffer;
         this._outputNode = outputNode
-        
-        this._bufferSource = this._context.createMediaElementSource(this._element)
-        const amp = new GainNode(this._context);
-        this._bufferSource.connect(amp).connect(this._outputNode);
     }
 
-    // sourceConnect() {
-    //     this._bufferSource = this._context.createMediaElementSource(this._element)
-    //     const amp = new GainNode(this._context);
-    //     this._bufferSource.connect(amp).connect(this._outputNode);
-    // }
-
-    playBeat() {
-        if (this._context.state === 'suspended') {
-            this._context.resume();
-        }
-
-        this._element.play();
+    addSchedule(start) {
+        const bufferSource = new AudioBufferSourceNode(this._context, {buffer: this._buffer});
+        const amp = new GainNode(this._context);
+        bufferSource.connect(amp).connect(this._outputNode);
+        bufferSource.start(start);
+        bufferSource.stop(start + 0.1);
     }
 }
 
